dafed2dc96f5908f8782c34d582857d9621f2d95
= <=>
2018-09-27 10:43:03 +0100

    Add termite configuration

--- home.nix
+++ home.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }:
 {
   manual.html.enable = true;
+
+  programs.termite = {
+    enable = true;
+    allowBold = true;
+    backgroundColor = "rgba(0, 0, 0, 0.5)";
+    font = "Monospace 9";
+  };
 }
