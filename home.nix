9aa14ce4ddf1bb22596005bb443d9dfbad1a527a
Gareth Charnock <=>
2018-09-27 21:40:01 +0100

    Give polybar a try

--- home.nix
+++ home.nix
@@ -8,4 +8,29 @@
     backgroundColor = "rgba(0, 0, 0, 0.5)";
     font = "Monospace 9";
   };
+
+  services.polybar = {
+    enable = true;
+    script = "polybar bar &";
+    config = {
+      "bar/top" = {
+        monitor = "\${env:MONITOR:VGA1}";
+        width = "100%";
+	height = "3%";
+	radius = 0;
+	modules-right = "date";
+	modules-center = "xworkspaces";
+      };
+      "module/date" = {
+        type = ''internal/date'';
+        internal = 5;
+        date = ''%d.%m.%y'';
+        time = ''%H:%M'';
+        label = ''%time%  %date%'';
+      };
+      "module/xworkspaces" = {
+        type = "internal/xworkspaces";
+      };
+    };
+  };
 }
