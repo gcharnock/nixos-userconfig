7c06a7d3ea34e3e804b2c4608369ef89d5fbbe42
Gareth Charnock <=>
2018-09-27 22:36:08 +0100

    Configure polybar a bit more

--- home.nix
+++ home.nix
@@ -12,25 +12,6 @@
   services.polybar = {
     enable = true;
     script = "polybar bar &";
-    config = {
-      "bar/top" = {
-        monitor = "\${env:MONITOR:VGA1}";
-        width = "100%";
-	height = "3%";
-	radius = 0;
-	modules-right = "date";
-	modules-center = "xworkspaces";
-      };
-      "module/date" = {
-        type = ''internal/date'';
-        internal = 5;
-        date = ''%d.%m.%y'';
-        time = ''%H:%M'';
-        label = ''%time%  %date%'';
-      };
-      "module/xworkspaces" = {
-        type = "internal/xworkspaces";
-      };
-    };
+    config = ./polybar.ini;
   };
 }
