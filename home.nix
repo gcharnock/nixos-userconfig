a88e76a130d53d53399bbb530cb8f5cf48d790ef
= <=>
2018-10-08 14:09:11 +0100

    Add some XMonad integration to polybar

--- home.nix
+++ home.nix
@@ -13,5 +13,18 @@
     enable = true;
     script = "polybar top &";
     config = ./polybar.ini;
+    extraConfig = ''
+      [module/workspaces-xmonad]
+      type = custom/script
+      exec = ${pkgs.coreutils}/bin/tail -F /tmp/.xmonad-workspace-log
+      #exec-if = [ -p /tmp/.xmonad-workspace-log ]
+      tail = true
+
+      [module/title-xmonad]
+      type = custom/script
+      exec = ${pkgs.coreutils}/bin/tail -F /tmp/.xmonad-title-log
+      # exec-if = [ -p /tmp/.xmonad-title-log ]
+      tail = true
+    '';
   };
 }
